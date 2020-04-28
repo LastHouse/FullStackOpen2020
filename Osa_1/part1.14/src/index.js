09d2a01702a66943f2d7439b3489fccf3a54345a
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-04-28 16:23:18 +0300

    part 1.14 done

---
+++ Osa_1/part1.14/src/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
+const App = () => {
+  const [anecdote, setAnecdote] = useState(anecdotes.map((item) => item));
+
+  const [selected, setSelected] = useState(0);
+
+  const [mostVotes, setMostVotes] = useState(0);
+
+  const [votes, setVotes] = useState(
+    Array.apply(null, { length: anecdotes.length }).map(function () {
+      return 0;
+    })
+  );
+
+  const toggleAnecdote = () => {
+    let result = anecdote[Math.floor(Math.random() * anecdote.length)];
+    let show = anecdote.indexOf(result);
+    setSelected(show);
+  };
+
+  const handleClick = () => {
+    let newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+    showMostVoted();
+  };
+
+  const showMostVoted = () => {
+    let result = votes.indexOf(Math.max.apply(null, votes));
+    setMostVotes(result);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdote[selected]}
+      <br></br>
+      <p>Has {votes[selected]} votes</p>
+      <button onClick={handleClick}>Vote</button>
+      <button onClick={toggleAnecdote}>Next anecdote</button>
+      <br></br>
+      <h1>Anecdote with the most votes</h1>
+      {anecdote[mostVotes]}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
