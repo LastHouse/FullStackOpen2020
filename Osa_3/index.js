14cccaabac0357fb15cd9a0dbe56893e372efe08
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-06-03 19:17:15 +0300

    backend changes

--- Osa_3/index.js
+++ Osa_3/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(
@@ -8,6 +9,7 @@ app.use(
     ':method :url :res[content-length] - :response-time ms :date[web] :person'
   )
 );
+app.use(cors());
 
 let persons = [
   {
@@ -144,7 +146,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
